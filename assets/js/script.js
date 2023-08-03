3cd60275e6fb24ad5da51f5f9bfa081b30fe2263
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-08-03 11:59:59 +0200

    setup structure to game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 console.log("connected");
+
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function calculateWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
